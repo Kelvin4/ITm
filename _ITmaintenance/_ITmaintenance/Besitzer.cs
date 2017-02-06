53b4d480ca81b448e9221ca17cbc3cecf66c56cd
Unknown <ksopnan@yahoo.fr>
2017-02-06 16:55:29 +0100

    LoginFertig

--- _ITmaintenance/_ITmaintenance/Besitzer.cs
+++ _ITmaintenance/_ITmaintenance/Besitzer.cs
@@ -7,8 +7,61 @@ namespace _ITmaintenance
 {
     class Besitzer
     {
-        string name, vorname, PLZ, ort, handy;
-        char straße, email;
-        int hausnummer;
+        private string name, vorname, postleitzahl, ort, handynummer;
+        private char strasse, email;
+        private int hausnummer;
+
+        public Besitzer(string name, string vorname, string postleitzahl, string ort, string handynummer, char strasse, char email, int hausnummer)
+        {
+            this.name = name;
+            this.vorname = vorname;
+            this.postleitzahl = postleitzahl;
+            this.ort = ort;
+            this.handynummer = handynummer;
+            this.strasse = strasse;
+            this.email = email;
+            this.hausnummer = hausnummer;
+        }
+
+        public string Name
+        {
+            get{return this.name;}
+        }
+
+        public string Vorname
+        {
+            get { return this.vorname; }
+        }
+
+        public string Postleitzahl
+        {
+            get { return this.vorname; }
+        }
+
+        public string Ort
+        {
+            get { return this.ort; }
+        }
+
+        public string Handynummer
+        {
+            get { return this.handynummer; }
+        }
+
+        public char Strasse        {
+            get { return this.strasse; }
+        }
+
+        public char Email
+        {
+            get { return this.email; }
+        }
+
+        public int Hausnummer
+        {
+            get { return this.hausnummer; }
+        }
+
+
     }
 }
--- _ITmaintenance/_ITmaintenance/Login.Designer.cs
+++ _ITmaintenance/_ITmaintenance/Login.Designer.cs
@@ -28,11 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tBMitarbeiterName = new System.Windows.Forms.TextBox();
+            this.tBPasswort = new System.Windows.Forms.TextBox();
+            this.lMitarbeiterName = new System.Windows.Forms.Label();
+            this.lMitarbeiterPasswort = new System.Windows.Forms.Label();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.bSchliessen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tBMitarbeiterName
+            // 
+            this.tBMitarbeiterName.Location = new System.Drawing.Point(121, 56);
+            this.tBMitarbeiterName.Name = "tBMitarbeiterName";
+            this.tBMitarbeiterName.Size = new System.Drawing.Size(100, 20);
+            this.tBMitarbeiterName.TabIndex = 0;
+            // 
+            // tBPasswort
+            // 
+            this.tBPasswort.Location = new System.Drawing.Point(121, 138);
+            this.tBPasswort.Name = "tBPasswort";
+            this.tBPasswort.Size = new System.Drawing.Size(100, 20);
+            this.tBPasswort.TabIndex = 1;
+            // 
+            // lMitarbeiterName
+            // 
+            this.lMitarbeiterName.AutoSize = true;
+            this.lMitarbeiterName.Location = new System.Drawing.Point(26, 56);
+            this.lMitarbeiterName.Name = "lMitarbeiterName";
+            this.lMitarbeiterName.Size = new System.Drawing.Size(82, 13);
+            this.lMitarbeiterName.TabIndex = 2;
+            this.lMitarbeiterName.Text = "Mitarbeitername";
+            this.lMitarbeiterName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lMitarbeiterPasswort
+            // 
+            this.lMitarbeiterPasswort.AutoSize = true;
+            this.lMitarbeiterPasswort.Location = new System.Drawing.Point(29, 144);
+            this.lMitarbeiterPasswort.Name = "lMitarbeiterPasswort";
+            this.lMitarbeiterPasswort.Size = new System.Drawing.Size(50, 13);
+            this.lMitarbeiterPasswort.TabIndex = 3;
+            this.lMitarbeiterPasswort.Text = "Passwort";
+            // 
+            // bLogin
+            // 
+            this.bLogin.Location = new System.Drawing.Point(130, 183);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(75, 23);
+            this.bLogin.TabIndex = 4;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = true;
+            // 
+            // bSchliessen
+            // 
+            this.bSchliessen.Location = new System.Drawing.Point(197, 230);
+            this.bSchliessen.Name = "bSchliessen";
+            this.bSchliessen.Size = new System.Drawing.Size(75, 23);
+            this.bSchliessen.TabIndex = 5;
+            this.bSchliessen.Text = "Schliessen";
+            this.bSchliessen.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 265);
+            this.Controls.Add(this.bSchliessen);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.lMitarbeiterPasswort);
+            this.Controls.Add(this.lMitarbeiterName);
+            this.Controls.Add(this.tBPasswort);
+            this.Controls.Add(this.tBMitarbeiterName);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tBMitarbeiterName;
+        private System.Windows.Forms.TextBox tBPasswort;
+        private System.Windows.Forms.Label lMitarbeiterName;
+        private System.Windows.Forms.Label lMitarbeiterPasswort;
+        private System.Windows.Forms.Button bLogin;
+        private System.Windows.Forms.Button bSchliessen;
     }
 }
